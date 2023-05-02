7e187dbb0f8933c62a6c2fd982a4dd00facca006
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-02 20:22:15 +0530

    SearchCssDone

--- client/src/components/chat/menu/Menu.jsx
+++ client/src/components/chat/menu/Menu.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import { Box } from '@mui/material';
+import Search from './Search';
 
 const Menu = () => {
   return (
    <Box>
     <Header/>
+    <Search  />
    </Box>
   )
 }
