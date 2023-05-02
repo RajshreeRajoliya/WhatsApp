d0ba6ba1af57b662a6e41f43e353d432392a5c03
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-03 00:47:23 +0530

    Createdenv

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
 const Connection = async () => {
-    const URL = 'mongodb://admin:admin123@ac-9dr7dfq-shard-00-00.xesp75j.mongodb.net:27017,ac-9dr7dfq-shard-00-01.xesp75j.mongodb.net:27017,ac-9dr7dfq-shard-00-02.xesp75j.mongodb.net:27017/?ssl=true&replicaSet=atlas-52tevd-shard-0&authSource=admin&retryWrites=true&w=majority';
+    const URL = `mongodb://${username}:${password}@ac-9dr7dfq-shard-00-00.xesp75j.mongodb.net:27017,ac-9dr7dfq-shard-00-01.xesp75j.mongodb.net:27017,ac-9dr7dfq-shard-00-02.xesp75j.mongodb.net:27017/?ssl=true&replicaSet=atlas-52tevd-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true});
         console.log('Database Connected Succesfully');
