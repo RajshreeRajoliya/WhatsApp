d9db6fee73b651115a6bbc92d88f4dcab5d8b9a8
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-02 19:49:02 +0530

    HandledCSSChatDialog

--- client/src/components/chat/ChatDialog.jsx
+++ client/src/components/chat/ChatDialog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Dialog, styled, Box } from '@mui/material';
-
+import Menu from './menu/Menu';
+import EmptyChat from './chat/EmptyChat';
 
 const dialogStyle = {
     height: '95%',
@@ -13,6 +14,21 @@ const dialogStyle = {
     overflow: 'hidden'
 };
 
+const Component = styled(Box)`
+    display: flex;
+`;
+    
+const LeftComponent = styled(Box)`
+    min-width: 450px;
+`;
+    
+const RightComponent = styled(Box)`
+    width: 73%;
+    min-width: 300px;
+    height: 100%;
+    border-left: 1px solid rgba(0, 0, 0, 0.14);
+`;
+
 const ChatDialog = () => {
   return (
     <Dialog 
@@ -21,6 +37,16 @@ const ChatDialog = () => {
     PaperProps={{ sx: dialogStyle }}
     maxWidth={'md'}
 >
+ <Component>
+                <LeftComponent>
+                    <Menu/>
+       </LeftComponent>
+                <RightComponent>
+                    
+   <EmptyChat />
+                    
+                </RightComponent>
+            </Component>
 </Dialog>
   )
 }
---
+++ client/src/components/chat/chat/EmptyChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmptyChat = () => {
+  return (
+    <div>
+      empty
+    </div>
+  )
+}
+
+export default EmptyChat
