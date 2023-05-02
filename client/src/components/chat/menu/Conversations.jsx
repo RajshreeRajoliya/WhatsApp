77dbc0252407aa73d2e0e750c89a27daa5ff015a
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-03 02:03:50 +0530

    handleMessagePart

--- client/src/components/chat/menu/Conversations.jsx
+++ client/src/components/chat/menu/Conversations.jsx
@@ -3,27 +3,42 @@ import React from 'react'
 import { getUsers } from '../../../service/api';
 import { Box, styled, Divider } from '@mui/material';
 import Conversation from './Conversation';
+import { AccountContext } from '../../../context/AccountProvider';
+
+const Component = styled(Box)`
+    overflow: overlay;
+    height: 81vh;
+`;
+const StyledDivider = styled(Divider)`
+    margin: 0 0 0 70px;
+    background-color: #e9edef;
+    opacity: .6;
+`;
 
 const Conversations = () => {
     const [users, setUsers] = useState([]);
-    
+    const {account} = useContext(AccountContext);
     useEffect(() => {
         const fetchData = async () => {
             let data = await getUsers();
             setUsers(data);
-            // setUsers(fiteredData);
+           
         }
         fetchData();
     }, []);
 
   return (
-    <Box>
+    <Component>
 {
     users.map(user => (
-        <Conversation user={user}/>
+        user.sub !== account.sub &&
+       <>
+       <Conversation user={user}/>
+       <StyledDivider/>
+       </>
     ))
 }
-    </Box>
+    </Component>
   )
 }
 
