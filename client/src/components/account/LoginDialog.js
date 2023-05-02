eaebc681505469ef39e6e0805883d7d4e023dc8b
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-02 17:12:54 +0530

    FixedCSS

--- client/src/components/account/LoginDialog.js
+++ client/src/components/account/LoginDialog.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Dialog, Typography , Box , List , ListItem } from '@mui/material'
+import { Dialog, Typography , Box , List , ListItem , styled } from '@mui/material'
 import { qrCodeImage } from '../../constants/data'
+
+
 const dialogStyle = {
     height : '96%',
     marginTop : '12%',
@@ -8,8 +10,40 @@ const dialogStyle = {
     maxWidth : '100%',
     maxHeight : '100%',
     boxShadow : 'none',
-    overflow : 'none',
+    overflow : 'hidden',
 }
+
+const Component = styled(Box)`
+display : flex;
+`
+
+const Container = styled(Box)`
+padding : 56px 0 56px 56px;
+`
+
+const QRCode = styled('img')({
+height : 264,
+width : 264,
+margin : '50px 0 0 50px'
+});
+
+const Title = styled(Typography)`
+font-size : 26px;
+color : #525252;
+font-weight : 300;
+font-family : inherit;
+margin-bottom : 25px;
+`
+
+const StyledList = styled(List)`
+& > li {
+    padding : 0;
+    margin-top : 15px;
+    font-size : 18px;
+    line-height : 28px;
+    color : #4a4a4a;
+}    
+`
 
 const LoginDialog = () => {
   return (
@@ -17,17 +51,20 @@ const LoginDialog = () => {
    open={true}
    PaperProps={{sx : dialogStyle}}
    >
-<Box>
-    <Box>
-        <Typography>To use WhatsApp on your computer:</Typography>
-        <List>
+<Component>
+    <Container>
+        <Title>To use WhatsApp on your computer:</Title>
+        <StyledList>
             <ListItem>1. Open WhatsApp on your phone</ListItem>
             <ListItem>2. Tap Menu Settings and select WhatsApp Web</ListItem>
             <ListItem>3. Point your phone to this screen to capture the code</ListItem>
-        </List>
-        <img src={qrCodeImage} alt='qrCode'/>
+        </StyledList>
+    </Container>
+
+    <Box>
+    <QRCode src={qrCodeImage} alt='qrCode'/>
     </Box>
-</Box>
+</Component>
    </Dialog>
   )
 }
