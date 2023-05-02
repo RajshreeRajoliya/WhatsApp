8c35df6ed5525d13b4f8662da2559ebd42457592
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-02 20:44:15 +0530

    MenuOptionCSSHandled

--- client/src/components/chat/menu/HeaderMenu.jsx
+++ client/src/components/chat/menu/HeaderMenu.jsx
@@ -3,6 +3,12 @@ import { MoreVert } from '@mui/icons-material';
 import { Menu, MenuItem, styled } from '@mui/material';
 import { useState} from 'react';
 
+const MenuOption = styled(MenuItem)`
+    font-size: 14px
+    padding: 15px 60px 5px 24px;
+    color: #4A4A4A;
+`;
+
 const HeaderMenu = () => {
     const [open, setOpen] = useState(false);
 
@@ -32,9 +38,8 @@ const HeaderMenu = () => {
                  horizontal: 'right',
                 }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuOption  onClick={handleClose}>Profile</MenuOption>
+       
       </Menu>
     </>
   )
