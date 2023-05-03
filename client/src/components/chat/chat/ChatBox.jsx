8f9ad388d3927cb53024d2335b6e66f62c32bb26
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-04 01:42:43 +0530

    ChatHandled

--- client/src/components/chat/chat/ChatBox.jsx
+++ client/src/components/chat/chat/ChatBox.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Box } from '@mui/material';
 import ChatHeader from './ChatHeader';
 import Messages from './Messages';
+import { useContext} from 'react';
+import { AccountContext } from '../../../context/AccountProvider';
+
 const ChatBox = () => {
+    const { person } = useContext(AccountContext);
   return (
     <Box style={{height: '75%'}}>
-      <ChatHeader/>
+      <ChatHeader person={person}/>
       <Messages/>
     </Box>
   )
--- client/src/components/chat/menu/Conversation.jsx
+++ client/src/components/chat/menu/Conversation.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { styled, Box, Typography } from "@mui/material";
+import {useContext} from 'react';
+import { AccountContext } from '../../../context/AccountProvider';
 
 const Component = styled(Box)`
     height: 45px;
@@ -17,9 +19,12 @@ const Image = styled('img') ({
 
 
 const Conversation = ({user}) => {
-  
+    const { setPerson } = useContext(AccountContext);
+  const getUser =()=> {
+setPerson(user);
+  } 
   return (
-   <Component>
+   <Component onClick={() => getUser()}>
 
                <Box>
                 <Image src={user.picture} alt="display picture" />
